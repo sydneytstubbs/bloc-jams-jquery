cbf4291db041062ba3d22bab78fe58968b12532c
Sydney Stubbs <sydneytstubbs@gmail.com>
2018-08-27 17:55:18 -0700

    Checkpoint 8 Complete 2

---
+++ scripts/song-list.js
@@ -0,0 +1,13 @@
+{
+  album.songs.forEach( (song, index) => {
+    song.element = $(`
+      <tr>
+      <td>${index + 1}</td>
+      <td>${song.title}</td>
+      <td>${song.duration}</td>
+      </tr>
+    `);
+
+    $('#song-list').append(song.element);
+  });
+}
