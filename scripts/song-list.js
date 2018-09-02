33f21c5b1b633dccb66949b968563507ea954e99
Sydney Stubbs <sydneytstubbs@gmail.com>
2018-09-01 19:04:21 -0700

    Song List Edit

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,9 +16,10 @@
 
     song.element.on('click', event => {
       player.playPause(song);
-      $('button#play-pause').attr('playState', player.playState);
+
     });
 
     $('#song-list').append(song.element);
+    
   });
 }
