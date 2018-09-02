2aab5afaff5f7941da0aed3b12a2c6a3bc9b6ebb
Sydney Stubbs <sydneytstubbs@gmail.com>
2018-09-01 17:26:45 -0700

    checkpoint 11 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,5 +13,27 @@ $('button#play-pause').on('click', function() {
     player.playPause(nextSong);
   });
 
+    $('button#previous').click(function() {
+      if(player.playState !== 'playing'){ return;}
 
-}
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      if(previousSongIndex < 0) { return; }
+      const previousSong = album.songs[previousSongIndex];
+      player.playPause(previousSong);
+    });
+
+    $('#time-control input').on('input', function (event) {
+       player.skipTo(event.target.value);
+   });
+
+    setInterval( () => {
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+      $('#time-control input').val(percent);
+    }, 1000);
+
+  }
